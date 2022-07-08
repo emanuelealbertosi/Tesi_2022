8e68592d32437627e50765c9ba58768c1844e6e5
Emanuele Albertosi <emanuele_albertosi@hotmail.com>
2022-07-08 15:08:31 +0200

    Public release

--- Services/ruleschk/app.py
+++ Services/ruleschk/app.py
@@ -1,21 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-#from flask_mysqldb import MySQL
+
 
 db = SQLAlchemy()
-#mysql = MySQL()
+
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.secret_key = 'secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://app:ubuntu@192.168.1.120/rules"
+    app.secret_key = 'xxxxxx'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://xxxxxxxx"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    #app.config['MYSQL_HOST'] = '192.168.1.120'
-    #app.config['MYSQL_USER'] = 'app'
-    #app.config['MYSQL_PASSWORD'] = 'Ubuntu123!'
-    #app.config['MYSQL_DB'] = 'dcc'
+
  
     db.init_app(app)
     
